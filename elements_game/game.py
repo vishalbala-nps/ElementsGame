43b69d53dbb7a000214925565f6d2df52a4d1bff
vishalbala-nps <vishal.bala.nps@gmail.com>
2020-01-15 14:37:27 +0530

    Implemented answer verification

--- elements_game/game.py
+++ elements_game/game.py
@@ -4,21 +4,42 @@ import json
 import random
 
 #Global Vars
-menu = ""
 elements = {}
 answer = ""
 answer_name = ""
 atomic_no = 0
+incorrect_attempts = 5
 #Game Functions
 def generate_no():
+    global incorrect_attempts
     global answer
     global atomic_no
     global answer_name
     global ano
+    incorrect_attempts = 5
     atomic_no = random.randint(1,118)
     answer = elements[atomic_no-1]['symbol']
     answer_name = elements[atomic_no-1]['name']
     ano.config(text=atomic_no)
+
+def checkans():
+    global noentry
+    global incorrect_attempts
+    global game
+    tans = noentry.get()
+    if tans == answer:
+        messagebox.showinfo("Correct Answer!","Correct Answer!! Keep going")
+        generate_no()
+        noentry.delete(0, 'end')
+    else:
+        incorrect_attempts = incorrect_attempts - 1
+        if incorrect_attempts <= 0 :
+            messagebox.showinfo("Game over!","Game over! you have answered wrongly more than 5 times!")
+            game.destroy()
+            home()
+        else:
+            messagebox.showinfo("Wrong Answer!","Wrong Answer!! Try again. You have "+str(incorrect_attempts)+" attempts left")
+            noentry.delete(0, 'end')
 
 def loadjson():
     global elements
@@ -30,7 +51,9 @@ def loadjson():
         exit(1)
 
 def startgame():
+    global game
     global ano
+    global noentry
     menu.destroy()
     game = Tk()
     game.title("Elements game")
@@ -41,11 +64,9 @@ def startgame():
     ano = Label(game,font=("Helvetica", 200))
     generate_no() 
     ano.place(x=400, y=250, anchor="center")
-    hint = Label(text="Insert Hint here", font=("Helvetica", 20))
-    hint.place(x=400, y=350, anchor="center")
     noentry = Entry(text="Atomic number",font=('Helvetica',25))
     noentry.place(x=350, y=400, anchor="center")
-    submit = Button(game, text='Submit', font=('Helvetica',25))
+    submit = Button(game, text='Submit', font=('Helvetica',25), command=checkans)
     submit.place(x=570, y=400, anchor="center")
     stop = Button(game, text='Stop Game', font=('Helvetica',25))
     stop.place(x=200, y=450, anchor="center")
